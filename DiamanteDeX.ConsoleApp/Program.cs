c504c9c30714f3f9ebb6ed7bea6f50189fbee468
oscar <oscarraichert@hotmail.com>
2022-03-06 15:35:25 -0300

    Adicionar arquivos de projeto.

---
+++ DiamanteDeX.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DiamanteDeX.ConsoleApp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Digite um número ímpar para fazer o diamante: ");
+            int maiorLinha = Convert.ToInt32(Console.ReadLine());
+
+            if (maiorLinha % 2 == 0)
+            {
+                Console.WriteLine("Diamante inválido!");
+                return;
+            }
+
+            int espacos = maiorLinha / 2;
+            for (int tamanhoLinha = 1; tamanhoLinha < maiorLinha; tamanhoLinha += 2)
+            {
+                EscreverLinha(tamanhoLinha, espacos--);
+            }
+
+            for (int tamanhoLinha = maiorLinha; tamanhoLinha > 0; tamanhoLinha -= 2)
+            {
+                EscreverLinha(tamanhoLinha, espacos++);
+            }
+        }
+
+        public static void EscreverLinha(int tamanhoLinha, int qntEspacos)
+        {
+            Console.WriteLine($"{new string(' ', qntEspacos)} {new string('x', tamanhoLinha)}");
+        }
+    }
+}
